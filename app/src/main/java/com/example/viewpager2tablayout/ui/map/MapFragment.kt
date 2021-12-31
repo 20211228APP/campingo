172ecb588c5a882922b45e0e5f1e37c1377c4261
Giyeong Kim <ggcc503@gmail.com>
2021-12-31 18:22:30 +0900

    Add googleMap
    - marker at malbourne

--- app/src/main/java/com/example/viewpager2tablayout/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/viewpager2tablayout/ui/map/MapFragment.kt
@@ -5,77 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment(), OnMapReadyCallback {
+//    private lateinit var mapView: MapView
 
-    private lateinit var mView: MapView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         var rootView = inflater.inflate(R.layout.fragment_map, container, false)
-
-        mView = rootView.findViewById(R.id.map) as MapView
-        mView.onCreate(savedInstanceState)
-        mView.getMapAsync(this)
-
+        setupMap()
         return rootView
     }
 
-
+    private fun setupMap() {
+        (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)!!.getMapAsync(this)
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val myLocation = LatLng(37.654601, 127.060530)
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(seoul))
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+        val seoul = LatLng(-37.813, 144.962)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(seoul))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(12f))
 
         //마커 출력
         val marker = MarkerOptions()
-            .position(myLocation)
+            .position(seoul)
             .title("Nowon")
             .snippet("노원역입니다.")
         googleMap?.addMarker(marker)
     }
-
-//
-//    override fun onStart() {
-//        super.onStart()
-//        mView.onStart()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        mView.onStop()
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        mView.onResume()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        mView.onPause()
-//    }
-//
-//    override fun onLowMemory() {
-//        super.onLowMemory()
-//        mView.onLowMemory()
-//    }
-//
-//    override fun onDestroy() {
-//        mView.onDestroy()
-//        super.onDestroy()
-//    }
-
-
 
 }
