ac9f10cf9f9bfd437cb318e1c0669dd554e3c850
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-01 11:06:18 +0900

    Phone call

--- app/src/main/java/com/example/viewpager2tablayout/ui/contact/ProfileAdapter.kt
+++ app/src/main/java/com/example/viewpager2tablayout/ui/contact/ProfileAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.viewpager2tablayout.ui.contact
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.viewpager2tablayout.R
 
@@ -19,6 +22,9 @@ class ProfileAdapter(private val dataset: ArrayList<ProfileItem>) :
         holder.name.text = dataset.get(position).NAME
         holder.age.text = dataset.get(position).AGE
         holder.call.text = dataset.get(position).CALL
+
+
+
     }
 
     override fun getItemCount(): Int {
@@ -29,5 +35,17 @@ class ProfileAdapter(private val dataset: ArrayList<ProfileItem>) :
         val name = itemView.findViewById<TextView>(R.id.tv_name)
         val age = itemView.findViewById<TextView>(R.id.tv_age)
         val call = itemView.findViewById<TextView>(R.id.tv_call)
+
+        init {
+            itemView.setOnClickListener {
+                val input = call.text.toString() // Uri를 이용해서 정보 저장
+                val myUri = Uri.parse("tel:${input}")
+
+                val myIntent = Intent(Intent.ACTION_DIAL, myUri) // 이동
+                itemView.context.startActivity(myIntent)
+
+            }
+
+        }
     }
 }
